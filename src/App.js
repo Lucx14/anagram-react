41206bfa92f565bace3f53da00b0964d0a161ef9
Lucien Najev <luciennajev@hotmail.com>
2018-12-20 17:22:58 +0000

    anagram generator in react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const API = 'http://codekata.com/data/wordlist.txt';
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+      this.state = {
+        dataSet: [],
+        isLoading: false,
+        error: null,
+        word: '',
+      }
+    }
+
+
+  componentDidMount() {
+    this.setState({ isLoading: true });
+    fetch(API)
+      .then(response => {
+        if (response.ok) {
+          return response.text();
+        } else {
+          throw new Error('Something went wrong...');
+        }
+      })
+      .then(data => this.setState({
+        dataSet: data.split('\n'),
+        isLoading: false,
+      }))
+      .catch(error => this.setState({ error, isLoading: false }));
+  }
+
+
+
+  getAnagrams = (word) => {
+    let result = [];
+    let sortedWord = this.formatWord(word);
+  
+    for (let i = 0; i < this.state.dataSet.length; i += 1) {
+      if (this.formatWord(this.state.dataSet[i]) === sortedWord && this.state.dataSet[i].toLowerCase() !== word.toLowerCase()) {
+        result.push(this.state.dataSet[i]);
+      }
+    }
+    if (result.length < 1 && word.length === 0) return 'Please enter a word to find its anagram!'
+    if (result.length < 1 && word.length > 0) return `There are no anagrams of ${word}`;
+    return `${word} has the following anagrams: ${result.join(', ')}`;
+  }
+
+
+  formatWord = (word) => {
+    return word
+      .replace(/['-]/g, '')
+      .toLowerCase()
+      .split('')
+      .sort()
+      .join('');
+  }
+
+  handleSearch = (event) => {
+    this.setState({
+      search: event.target.value,
+      word: event.target.value,
+    });
+  }
+
+
+
   render() {
+    const { isLoading, error } = this.state;
+
+    if (error) return <p>{error.message}</p>
+    if (isLoading) return <p>Loading...</p>
+    
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Anagram Generator</h1>
+        <input
+              type="text"
+              placeholder="word..."
+              value={this.state.search}
+              onChange={this.handleSearch} />
+        <p>{this.getAnagrams(this.state.word)}</p>
       </div>
     );
   }
